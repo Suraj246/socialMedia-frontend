1283b1708386d419954b803eff2f41370aa45d65
Suraj246 <surajsurwase9881@gmail.com>
2023-06-25 12:58:24 +0530

    created middleware

--- src/middleware.js
+++ src/middleware.js
@@ -4,10 +4,11 @@ export function middleware(req) {
     let cookie = req.cookies.get('user')
     // console.log(cookie)
     const url = req.nextUrl.clone()
-    // console.log(url.pathname)
+    console.log(url.pathname)
+    console.log(url.origin)
 
 
-    if (!cookie && url.pathname === '/') {
+    if (!cookie && url.pathname === 'https://social-media-frontend-tan.vercel.app/') {
         return NextResponse.rewrite(`${url.origin}/login`)
     }
     return NextResponse.next()
